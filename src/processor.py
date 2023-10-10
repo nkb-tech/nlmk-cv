ff6290a74f2f8bcd50b10702639cfb90bd366fb2
Alexander Nevarko <nevarko.ad@phystech.edu>
2023-10-10 12:04:53 +0300

    Initial

---
+++ src/processor.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import logging
+
+class GeneralCamProcessor():
+    '''
+    General camera CV processor
+    '''
+    def __init__(self, cam_cfg: pd.DataFrame,
+                 ):
+        pass
